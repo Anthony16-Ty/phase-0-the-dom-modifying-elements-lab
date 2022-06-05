673c08a4623d658d5e0468f63d22ba9420cb194a
Anthony Kimeu <tkymeu@gmail.com>
2022-06-05 23:45:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const element = document.getElementById('main');
+
+element.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML =`Anthony is the champion`;
